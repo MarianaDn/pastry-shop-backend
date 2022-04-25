a60389300e7cb971a4a8bd27122ffe1553b8f25e
mdenys <mdenys@griddynamics.com>
2022-04-25 22:20:06 +0300

    change auth path

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-// const stripeRoute = require("./routes/stripe");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
@@ -20,13 +21,14 @@ mongoose
   .then(() => console.log("DB Connection Successfull"))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-// app.use("/api/checkout", stripeRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
