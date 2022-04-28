1b0796b5e7e63c8257f03ed5f657d967a70b4986
mdenys <mdenys@griddynamics.com>
2022-04-28 11:04:41 +0300

    some fix

--- server/routes/order.js
+++ server/routes/order.js
@@ -7,7 +7,7 @@ const {
 } = require("./verifyToken");
 
 //CREATE
-router.post("/", verifyToken, async (req, res) => {
+router.post("/", async (req, res) => {
   const newOrder = new Order(req.body);
 
   try {
